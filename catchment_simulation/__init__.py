3d9169ca6c474d1532103a5eaf3328bf54b0a5de
BuczynskiRafal <buczynski.rafal2@gmail.com>
2026-01-16 06:54:34 +0100

    Migrate packaging from setup.py to pyproject.toml

---
+++ catchment_simulation/__init__.py
@@ -0,0 +1,6 @@
+"""Package for simulating subcatchment features with Storm Water Management Model."""
+
+from .catchment_features_simulation import FeaturesSimulation
+
+__all__ = ["FeaturesSimulation"]
+__version__ = "0.0.7"
